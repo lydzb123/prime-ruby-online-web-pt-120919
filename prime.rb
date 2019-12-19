0384763a9c2676cd942c840d395f82b2dcc9d1a9
Lydia B <lydiabaik@gmail.com>
2019-12-19 04:03:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(int)
   if int <= 1
     return false
   elsif
-    int.even? && int != 2 || (2...int).each {|i| int % i == 0}
+    int != 2 || (2...int).each {|i| int % i == 0}
     return false
   else
     return true
