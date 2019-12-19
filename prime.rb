566edd96fd8b2cbd09c918ee853232000866ce98
Lydia B <lydiabaik@gmail.com>
2019-12-19 04:04:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  elsif
+    int != 2 && (2...int).any? {|i| int % i == 0}
+    return false
+  else
+    return true
+  end
+end
