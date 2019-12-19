779007d58a5af8b842a6672bc3286e79386c8c19
Lydia B <lydiabaik@gmail.com>
2019-12-19 04:00:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(n)
     if n <= 1
       return false
-    else 
+    else
         if (2...n).any? { |i| n % i  == 0}
             return false
         else
             return true
-        end 
+        end
     end
 end
