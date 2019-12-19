ba0ab673ab405d6414c3efffebdde7fad0f6370f
Lydia B <lydiabaik@gmail.com>
2019-12-19 04:01:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(int)
   if int <= 1
     return false
   else
-    if int != 2 && (2...int).each {|i| int % i == 0}
+    if (2...int).each {|i| int % i == 0}
       return false
     else
     return true
